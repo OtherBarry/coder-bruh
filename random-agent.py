78699648e1e65e72775a0b736b362222ddd0c642
OtherBarry <charlie.wilson4@gmail.com>
2020-12-12 10:19:39 +1100

    Create random-agent.py

---
+++ random-agent.py
@@ -0,0 +1,10 @@
+import random
+
+
+class Agent:
+
+    def __init__(self):
+        self.actions = ['u', 'd', 'l', 'r', 'b']
+
+    def next_move(self, game_state, player_state):
+        return random.choice(self.actions)
