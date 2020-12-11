2c6d5830f9741c74b65b955f21dbc0c0550eeca0
OtherBarry <charlie.wilson4@gmail.com>
2020-12-12 10:05:30 +1100

    Create primary-agent.py

---
+++ primary-agent.py
@@ -0,0 +1,12 @@
+"""File for primary agent"""
+
+class Agent:
+    """Class for primary agent"""
+
+    def __init__(self, player_num, env):
+        """your agent's initialization code goes here, if any"""
+        pass
+
+    def next_move(self, game_state, player_state):
+        """This method is called each time the player needs to choose an action"""
+        pass
