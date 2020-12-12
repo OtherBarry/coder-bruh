20efb563251e4cb49c8b48adf39f3308c77dcbdb
OtherBarry <charlie.wilson4@gmail.com>
2020-12-12 11:32:28 +1100

    Implement tracking bombs

--- primary-agent.py
+++ primary-agent.py
@@ -14,6 +14,7 @@ class Agent:
         self.tick_number = -1
         self.updated = True
         self.action_queue = []
+        self.bombs = {}
 
     def next_move(self, game_state, player_state):
         """This method is called each time the player needs to choose an action"""
@@ -22,6 +23,19 @@ class Agent:
         if not updated:
             return self.action_queue.pop()
         return None
+
+    def track_bombs(self, bombs):
+        detonation_tick = self.tick_number - 35
+        for location, tick in self.bombs.items():
+            if tick == detonation_tick:
+                self.track_detonation(location)
+                del self.bombs[location]
+        for bomb in bombs:
+            if bomb not in self.bombs:
+                self.bombs[bomb] = self.tick_number
+
+    def track_detonation(self, location):
+        pass
 
     def calculate_distance(self):
         pass
